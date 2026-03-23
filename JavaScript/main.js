9de41f9ac5564e31fa01587c06f2d80cf810a803
GutsCarioka <modestiagabriel@gmail.com>
2026-03-23 19:56:38 -0300

    Update HTML structure and enhance JavaScript for movie search functionality

--- JavaScript/main.js
+++ JavaScript/main.js
@@ -2,19 +2,14 @@
 
 const API_KEY = "8f983316167b81840e3ffdcc9f572aa4";
 const BASE_URL = "https://api.themoviedb.org/3";
-var number_movie = 5;
-const pesq1 = window.document.getElementById("pesq");
-function pesquisa() {
-  console.log = pesq1;
-}
-
-async function getPopularMovies() {
+async function getPopularMovies(number_movie) {
   try {
     const response = await fetch(
       `${BASE_URL}/movie/popular?api_key=${API_KEY}&language=pt-BR`,
     );
     const data = await response.json();
     const movies = data.results[number_movie];
+    const moviesTitles = movies.title;
     const page = data.page;
 
     const title1 = window.document.getElementById("title");
@@ -26,6 +21,9 @@ async function getPopularMovies() {
     const releaseDate1 = window.document.getElementById("release_date");
     releaseDate1.innerHTML = movies.release_date;
 
+    const subtitle1 = window.document.getElementById("subtitle");
+    subtitle1.innerHTML = movies.overview;
+
     window.document.getElementById("poster_path").src =
       `https://image.tmdb.org/t/p/w500${movies.poster_path}`;
 
@@ -35,4 +33,11 @@ async function getPopularMovies() {
   }
 }
 
-getPopularMovies();
+getPopularMovies(0);
+
+const botao = document.getElementById("meuBotao");
+botao.onclick = function () {
+  const pesquisa = document.getElementById("pesq");
+  const valorDigitado = pesquisa.value;
+  getPopularMovies(valorDigitado);
+};
