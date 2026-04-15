1776cc5e335679fa8f467cfc531a0ff3354860e5
GutsCarioka <modestiagabriel@gmail.com>
2026-04-14 22:47:37 -0300

    Enhance movie search functionality and update styles for results display

--- JavaScript/main.js
+++ JavaScript/main.js
@@ -2,43 +2,110 @@
 
 const API_KEY = "8f983316167b81840e3ffdcc9f572aa4";
 const BASE_URL = "https://api.themoviedb.org/3";
-async function getPopularMovies(number_movie) {
+//Variavel Global que vai armazenar a lista de filmes populares retornada pela API, permitindo que seja acessada e manipulada em diferentes partes do código conforme necessário.
+let listaFilmes = [];
+async function getPopularMovies() {
   try {
     const response = await fetch(
       `${BASE_URL}/movie/popular?api_key=${API_KEY}&language=pt-BR`,
     );
+
     const data = await response.json();
-    
-    const movies = data.results[number_movie];
-    const page = data.page;
-    const subtile1 = window.document.getElementById("subtitle");
-   subtile1.innerHTML = movies.overview;
-
-    const title1 = window.document.getElementById("title");
-    title1.innerHTML = movies.title;
-    const OriginalTitle1 = window.document.getElementById("original_title");
-    OriginalTitle1.innerHTML = movies.original_title;
-
-    const releaseDate1 = window.document.getElementById("release_date");
-    releaseDate1.innerHTML = movies.release_date;
-
-    window.document.getElementById("poster_path").src =
-      `https://image.tmdb.org/t/p/w500${movies.poster_path}`;
-    
-    window.document.getElementById("Backdrop_path").src =
-      `https://image.tmdb.org/t/p/w500${movies.backdrop_path}`;
-
-    // console.log(data.results[0]);
+    listaFilmes = data.results;
+    mostrarFilme(listaFilmes[0]);
   } catch (error) {
     console.error("error ao buscar filmes populares:", error);
   }
 }
 
-getPopularMovies(0);
+function mostrarFilme(movie) {
+  document.getElementById("subtitle").innerHTML = movie.overview;
 
-const botao = document.getElementById("meuBotao");
+  window.document.getElementById("title").innerHTML = movie.title;
+
+  window.document.getElementById("original_title").innerHTML =
+    movie.original_title;
+
+  window.document.getElementById("release_date").innerHTML = movie.release_date;
+
+  window.document.getElementById("poster_path").src =
+    `https://image.tmdb.org/t/p/w500${movie.poster_path}`;
+
+  //window.document.getElementById("Backdrop_path").src =
+  //`https://image.tmdb.org/t/p/w500${movie.backdrop_path}`;
+}
+//Function de requisicao de filmes para pesquisa
+async function buscarFilmes(query) {
+  try {
+    const response = await fetch(
+      `${BASE_URL}/search/movie?api_key=${API_KEY}&query=${encodeURIComponent(query)}`,
+    );
+
+    const data = await response.json();
+
+    if (!data.results || data.results.length === 0) {
+      document.getElementById("resultados").innerHTML =
+        "<p>Nenhum filme encontrado</p>";
+      return;
+    }
+
+    mostrarResultados(data.results);
+  } catch (error) {
+    console.error("Erro ao seguir com a pesquisa de filmes:", error);
+  }
+}
+
+//função de Busca de filmes
+
+function mostrarResultados(filmes) {
+  if (!filmes || filmes.length === 0) return;
+
+  const container = document.getElementById("resultados");
+  container.innerHTML = "";
+
+  filmes.slice(0, 5).forEach((filme) => {
+    const div = document.createElement("div");
+    div.textContent = filme.title;
+
+    div.style.cursor = "pointer";
+
+    div.onclick = () => {
+      mostrarFilme(filme);
+      container.innerHTML = ""; // limpa sugestões
+    };
+
+    container.appendChild(div);
+  });
+}
+
+input.addEventListener("input", () => {
+  clearTimeout(timeout);
+
+  const valor = input.value;
+
+  if (valor.length <= 2) {
+    document.getElementById("resultados").innerHTML = "";
+    return;
+  }
+
+  timeout = setTimeout(() => {
+    buscarFilmes(valor);
+  }, 300);
+});
+
+/*const botao = document.getElementById("meuBotao");
+
 botao.onclick = function () {
-  const pesquisa = document.getElementById("pesq");
-  const valorDigitado = pesquisa.value;
-  getPopularMovies(valorDigitado);
+  const valorDigitado = document.getElementById("pesq").value.toLowerCase();
+  const filtrados = listaFilmes.filter((filme) =>
+    filme.title.toLowerCase().includes(valorDigitado),
+  );
+
+  if (filtrados.length > 0) {
+    mostrarFilme(filtrados[0]); //Mostra primeiro filme da lista filtrada
+  } else {
+    alert("Filme Não Encontrado, Ou ele não é popular ou ele é ruim!");
+  }
 };
+*/
+getPopularMovies();
